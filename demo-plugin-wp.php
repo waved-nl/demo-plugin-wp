92963c7bbab9d7248a0aceac8446550ad2cc459f
Max Swinkels <max.swinkels@outlook.com>
2024-02-14 18:44:51 +0100

    :tada: initial commit

---
+++ demo-plugin-wp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Demo plugin
+ * Plugin URI: https://github.com/waved-nl/demo-plugin-wp
+ * Description: This is a demo plugin
+ * Version: 0.1
+ * Author: Waved
+ * Author URI: https://waved.nl
+ * License: GPL2
+ */
